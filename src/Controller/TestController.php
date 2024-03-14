2d6d5e8f0f5f82feff4f0ef5a476f6e89f7c71e0
Erison Suzuki <erison_suzuki@apple.com>
2024-03-14 10:31:12 -0300

    feat: using request query

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,14 +2,21 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
     #[Route('/test')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Test!');
+        return new Response(
+            '<h1>id</h1>: ' . $request->query->get('id'),
+            401,
+            [
+                'Header-test' => 'Test'
+            ]
+        );
     }
 }
