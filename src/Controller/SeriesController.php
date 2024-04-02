229dfab4a63a0ddb3dddb0380405910ca1d54e8b
Erison Suzuki <erison_suzuki@apple.com>
2024-04-02 08:00:09 -0300

    feat: using variables in the view

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -4,20 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SeriesController extends AbstractController
 {
-    #[Route('/series', name: 'app_series')]
+    #[Route('/series', name: 'app_series', methods: ['GET'])]
     public function index(): Response
     {
         $seriesList = ['Lost', 'Grey\'s Anatomy', 'Loki',];
 
-        return new JsonResponse($seriesList);
 
         return $this->render('series/index.html.twig', [
-            'controller_name' => 'SeriesController',
+            'seriesList' => $seriesList,
         ]);
     }
 }
